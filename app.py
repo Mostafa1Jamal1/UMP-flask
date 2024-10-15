821b2fbfdd54a9b930e03086b2e6c0ea3bf8fc87
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-10-15 06:37:06 +0300

    feat: app to test the UMP package

---
+++ app.py
@@ -0,0 +1,18 @@
+# from UMP_flask import configure_app
+from UMP_flask import configure_app
+from flask import Flask
+
+
+if __name__ == '__main__':
+    # run application (can also use flask run)
+    app = Flask(__name__)
+    configure_app.basic_auth(app=app, debug=True,
+                             SECURITY_REGISTERABLE=True,
+                             )
+    # configure_app.mail(app=app,
+    #                           SECURITY_CONFIRMABLE=True,
+    #                           SECURITY_RECOVERABLE=True,
+    #                           SECURITY_CHANGEABLE=True)
+    security = configure_app.security(app=app)
+    # Run Flask
+    app.run()
