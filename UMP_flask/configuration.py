f15093cbae2949632bc00ec142893ff631f5e719
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-10-16 03:35:11 +0300

    feat(config): add with_mail function for configuring email settings

--- UMP_flask/configuration.py
+++ UMP_flask/configuration.py
@@ -1,7 +1,7 @@
 """
 UMP Flask Application Configuration
 
-This module provides configuration settings and functions for the UMP Flask application.
+This module provides configuration settings and functions for the Flask app.
 """
 
 from os import environ as env
@@ -51,6 +51,54 @@ class configure_app():
         app.register_blueprint(blueprint)
         return app
 
+    def with_mail(app: Flask,
+                  SECURITY_CONFIRMABLE: Optional[bool] = False,
+                  SECURITY_RECOVERABLE: Optional[bool] = False,
+                  SECURITY_CHANGEABLE: Optional[bool] = False,
+                  ) -> Flask:
+        """
+        Configure the Mail features for the given app.
+
+        Args:
+            app: The Flask application instance to configure.
+            SECURITY_CONFIRMABLE: Whether to enable email confirmation.
+            SECURITY_RECOVERABLE: Whether to enable password recovery.
+            SECURITY_CHANGEABLE: Whether to enable password changes.
+
+        Returns:
+            The configured app.
+        """
+        # for security these variables should be set in .env
+        assert env.get('MAIL_SERVER') is not None, \
+            "MAIL_SERVER is not set in .env file.\n" \
+            "Configuration variables should be set in the .env file."
+        assert env.get("MAIL_PORT") is not None, \
+            "MAIL_PORT is not set in .env file.\n" \
+            "Configuration variables should be set in the .env file."
+        assert str(env.get("MAIL_PORT")).isdigit(), \
+            "MAIL_PORT is not a number (for example 25, 465 or 587)"
+        assert not (env.get('MAIL_USE_TLS') == "True" and
+                    env.get('MAIL_USE_SSL') == "True"), \
+            "MAIL_USE_TLS and MAIL_USE_SSL cannot be both set to True"
+        assert env.get('MAIL_USERNAME') is not None, \
+            "MAIL_USERNAME is not set in .env file.\n" \
+            "Configuration variables should be set in the .env file."
+        assert env.get('MAIL_PASSWORD') is not None, \
+            "MAIL_PASSWORD is not set in .env file.\n" \
+            "Configuration variables should be set in the .env file."
+
+        app.config['MAIL_SERVER'] = env.get("MAIL_SERVER")
+        app.config['MAIL_PORT'] = env.get("MAIL_PORT")
+        app.config['MAIL_USE_TLS'] = env.get("MAIL_USE_TLS") == "True"
+        app.config['MAIL_USE_SSL'] = env.get("MAIL_USE_SSL") == "True"
+        app.config['MAIL_USERNAME'] = env.get("MAIL_USERNAME")
+        app.config['MAIL_PASSWORD'] = env.get("MAIL_PASSWORD")
+
+        app.config["SECURITY_CONFIRMABLE"] = SECURITY_CONFIRMABLE
+        app.config["SECURITY_RECOVERABLE"] = SECURITY_RECOVERABLE
+        app.config["SECURITY_CHANGEABLE"] = SECURITY_CHANGEABLE
+        mail = Mail(app)
+        return app
 
     def security(app: Flask) -> Security:
         """
