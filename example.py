a8595471a02811b91ec95326835d4e12d0932880
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-10-17 22:12:26 +0300

    style(example.py): fix indentation

--- example.py
+++ example.py
@@ -7,12 +7,11 @@ if __name__ == '__main__':
     # run application (can also use flask run)
     app = Flask(__name__)
     configure_app.basic_auth(app=app, debug=True,
-                             SECURITY_REGISTERABLE=True,
-                             )
+                             SECURITY_REGISTERABLE=True)
     configure_app.with_mail(app=app,
-                              SECURITY_CONFIRMABLE=True,
-                              SECURITY_RECOVERABLE=True,
-                              SECURITY_CHANGEABLE=True)
+                            SECURITY_CONFIRMABLE=True,
+                            SECURITY_RECOVERABLE=True,
+                            SECURITY_CHANGEABLE=True)
     security = configure_app.security(app=app)
     # Run Flask
     app.run()
